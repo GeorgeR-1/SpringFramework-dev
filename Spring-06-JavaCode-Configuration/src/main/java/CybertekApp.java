976cd1da9f680e0b0ded947c39bba016293a6024
GeorgeLondon1 <radac.george@yahoo.com>
2020-10-24 19:59:15 +0100

    Java Configuration - application.properties

---
+++ Spring-06-JavaCode-Configuration/src/main/java/CybertekApp.java
@@ -0,0 +1,19 @@
+import com.cybertek.configs.CybertekAppConfig;
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(CybertekAppConfig.class);
+
+        Course course = container.getBean("java",Course.class);
+
+        course.getTeachingHours();
+
+        System.out.println(course.toString());
+
+
+    }
+}
