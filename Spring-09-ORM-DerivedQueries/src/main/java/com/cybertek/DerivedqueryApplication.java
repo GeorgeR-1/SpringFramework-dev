3b425339d4b6acd1f8e8d271924e9518f79340cd
GeorgeLondon1 <radac.george@yahoo.com>
2020-12-23 04:06:45 +0200

    SpringBoot ORM - Derived Queries

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
@@ -1,5 +1,6 @@
 package com.cybertek;
 
+import com.cybertek.repository.EmployeeRepository;
 import com.cybertek.repository.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +14,9 @@ public class DerivedqueryApplication {
     @Autowired
     RegionRepository regionRepository;
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(DerivedqueryApplication.class, args);
     }
@@ -22,22 +26,26 @@ public class DerivedqueryApplication {
         System.out.println("------------------ Region start --------------------");
         System.out.println("findByCountry: " + regionRepository.findByCountry("Canada"));
         System.out.println("findDistinctByCountry: " + regionRepository.findDistinctByCountry("Canada"));
-        System.out.println("findDistinctByCountry: " + regionRepository.findByCountryContaining("United"));
-        System.out.println("findDistinctByCountry: " +
+        System.out.println("findByCountryContaining: " + regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainingOrderByCountry: " +
                 regionRepository.findByCountryContainingOrderByCountry("United"));
-        System.out.println("findDistinctByCountry: " + regionRepository.findTop2ByCountry("United"));
-
-
-
-
-
-
-
-
-
-
+        System.out.println("findTop2ByCountry: " + regionRepository.findTop2ByCountry("United"));
 
         System.out.println("------------------ Region end --------------------");
+    }
+
+    @PostConstruct
+    public void testEmployees(){
+        System.out.println("------------------ Employee start --------------------");
+
+        System.out.println("findByEmail()" + employeeRepository.findByEmail("bmanueau0@dion.ne.jp"));
+        System.out.println("findByFirstNameAndLastNameOrEmail: " +
+                employeeRepository.findByFirstNameAndLastNameOrEmail("name","sdad","email"));
+        System.out.println("findByFirstNameStartingWith: " +
+                employeeRepository.findByFirstNameStartingWith("J"));
+        System.out.println("findByEmailIsNull()" + employeeRepository.findByEmailIsNull());
+
+        System.out.println("------------------ Employee end --------------------");
     }
 
 }
--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.cybertek.repository;
 
 import com.cybertek.entity.Employee;
+import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -12,7 +15,32 @@ public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     //Display all employees with email address ''
     List<Employee> findByEmail(String email);
 
-    List<Employee> findByFirstNameAndAndLastNameOrEmail(String firstName,String lastName,String email);
+    //Display all employees with first name '', also show all employees with a email address ''
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstName,String lastName,String email);
+
+    //Display all employees that first name is not ''
+    List<Employee> findByFirstNameIsNot(String firstName);
+
+    //Display all employees where last name starts with ''
+    List<Employee> findByFirstNameStartingWith(String pattern);
+
+    //Display all employees with salaries greater than ''
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    //Display all employees with salaries less than ''
+    List<Employee> findBySalaryLessThan(Integer salary);
+
+    //Display all employees that have been hired between '' and ''
+    List<Employee> findByHireDateBetween(LocalDate startDate,LocalDate endDate);
+
+    //Display all employees where salaries greater and equal to '' in order
+    List<Employee> findBySalaryIsGreaterThanEqualOrderBySalaryDesc(Integer salary);
+
+    //Display top unique 3 employees that is making less than ''
+    List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
+
+    //Display all employees that do not have email address
+    List<Employee> findByEmailIsNull();
 
 
 
