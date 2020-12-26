c1c0e3ae11d064666ae6096797d95eb04cae1b89
GeorgeLondon1 <radac.george@yahoo.com>
2020-12-26 18:36:44 +0200

    SpringBoot ORM - JPQL - Derived Queries-JPQL Queries-Native Queries - Lab

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/CinemaappApplication.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/CinemaappApplication.java
@@ -1,13 +1,24 @@
 package com.cybertek;
 
+import com.cybertek.repository.CinemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CinemaappApplication {
+
+    @Autowired
+    CinemaRepository cinemaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaappApplication.class, args);
     }
 
+    @PostConstruct
+    public void runner(){
+        System.out.println(cinemaRepository.findAllByLocation_Country("United States"));
+    }
 }
