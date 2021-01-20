03ab951c433e9d1fa8f8e8cf62aaef134f0937da
GeorgeLondon1 <radac.george@yahoo.com>
2021-01-20 03:35:00 +0200

    SpringBoot Security - Form Authorization

--- Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/SecurityConfiguration.java
+++ Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.cybertek.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,16 +8,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserPrincipalDetailsService userPrincipalDetailsService;
+
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests() //request should be authorised
-                .antMatchers("index.html").permitAll()
+                //.antMatchers("index.html").permitAll()
                 .antMatchers("/profile/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/management/**").hasAnyAuthority("ADMIN","MANAGER")
@@ -24,7 +31,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .defaultSuccessUrl("/index")
-                .permitAll();
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout=true")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(120)
+                .key("cybertekSecret")
+                .userDetailsService(userPrincipalDetailsService);
 
     }
 
