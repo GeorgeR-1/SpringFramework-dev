01788369f77ff11677d3219fcb59f77f97222b9b
GeorgeLondon1 <radac.george@yahoo.com>
2021-01-27 02:02:34 +0200

    Spring REST - HttpVerbs Legacy

--- Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/controller/ProductController.java
@@ -2,13 +2,12 @@ package com.cybertek.controller;
 
 import com.cybertek.entity.Product;
 import com.cybertek.service.ProductService;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Controller
 public class ProductController {
 
     private ProductService productService;
@@ -19,19 +18,36 @@ public class ProductController {
 
     @RequestMapping(value = "/products/{id}",method = RequestMethod.GET)
     public @ResponseBody Product getProduct(@PathVariable("id") long id){
+
         return productService.getProduct(id);
     }
 
     @RequestMapping(value = "/products",method = RequestMethod.GET)
     public @ResponseBody List<Product> getProducts(){
+
         return productService.getProducts();
     }
 
-    //CREATE PRODUCT
 
-    //DELETE PRODUCT
+    @RequestMapping(value = "/products",method = RequestMethod.POST)
+    public @ResponseBody List<Product> createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
 
-    //UPDATE PRODUCT
+    @RequestMapping(value = "/products/{id}",method = RequestMethod.DELETE)
+    public @ResponseBody List<Product> deleteProduct(@PathVariable("id") long id){
+        return productService.delete(id);
+    }
+
+    @RequestMapping(value = "/products/{id}",method = RequestMethod.PUT)
+    public @ResponseBody List<Product> updateProduct(@PathVariable("id") long id,
+                                                     @RequestBody Product product){
+
+        return productService.updateProduct(id,product);
+
+    }
+
+
 
 
 
