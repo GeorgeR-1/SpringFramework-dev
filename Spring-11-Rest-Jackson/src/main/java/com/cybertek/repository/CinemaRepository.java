8b1de1b350c36bc30b1915381fa3fdfc17dee5c7
GeorgeLondon1 <radac.george@yahoo.com>
2021-01-27 04:22:12 +0200

    Spring REST - Jackson

---
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -0,0 +1,49 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.Cinema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CinemaRepository extends JpaRepository<Cinema,Long> {
+    // ------------------- DERIVED QUERIES ------------------- //
+//Write a derived query to get cinema with a specific name
+    Optional<Cinema> findByName(String name);
+//Write a derived query to read sorted the top 3 cinemas that
+// contains a specific sponsored name
+    Optional<Cinema> findFirst3ByNameContaining(String pattern);
+//Write a derived query to list all cinemas in a specific country
+    Optional<Cinema> findAllByLocation_Country(String country);
+//Write a derived query to list all cinemas with a specific
+// name or sponsored name
+    Optional<Cinema> findAllByNameOrSponsoredName(String name,String sponsoredName);
+
+    // ------------------- JPQL QUERIES ------------------- //
+//Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name FROM Cinema c WHERE c.id =:id")
+    String findCinemaById(Long id);
+    // ------------------- Native QUERIES ------------------- //
+//Write a native query to read all cinemas by location country
+    @Query(value = "SELECT * FROM cinema c inner join location l " +
+            "on l.id =c.id where l.country=?1",
+            nativeQuery = true)
+    List<Cinema> fetchCinemasByLocationCountry(String locationCountry);
+//Write a native query to read all cinemas by name or
+// sponsored name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema where " +
+            "name ilike concat('%',:pattern,'%') or " +
+            "sponsored_name ilike concat('%',:pattern,'%')",
+            nativeQuery = true)
+    List<Cinema> retrieveAllCinemasByNameOrSponsoredName(String pattern);
+//Write a native query to sort all cinemas by name
+    @Query(value = "SELECT * FROM cinema ORDER BY name",
+    nativeQuery = true)
+    List<Cinema> sortByName();
+//Write a native query to distinct all cinemas by sponsored name
+    @Query(value = "SELECT DISTINCT sponsored_name FROM cinema",
+    nativeQuery = true)
+    List<String> distinctBySponsoredName();
+
+}
---
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/repository/MovieCinemaRepository.java
@@ -0,0 +1,47 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.MovieCinema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface MovieCinemaRepository extends JpaRepository<MovieCinema,Long> {
+
+    // ------------------- DERIVED QUERIES ------------------- //
+//Write a derived query to read movie cinema with id
+    Optional<MovieCinema> findMovieCinemaById(Long id);
+//Write a derived query to count all movie cinemas with a specific cinema id
+    Integer countAllByCinemaId(Long id);
+//Write a derived query to count all movie cinemas with a specific movie id
+    Integer countAllByMovieId(Long id);
+//Write a derived query to list all movie cinemas with higher
+// than a specific date
+    List<MovieCinema> findAllByDateTimeAfter(LocalDateTime dateTime);
+//Write a derived query to find the top 3 expensive movies
+    List<MovieCinema> findFirst3byOrderByMoviePriceDesc();
+//Write a derived query to list all movie cinemas that contains
+// a specific movie name
+    List<MovieCinema> findAllByMovieNameContaining(String pattern);
+//Write a derived query to list all movie cinemas in a specific location
+    List<MovieCinema> findAllByCinemaLocationName(String name);
+
+    // ------------------- JPQL QUERIES ------------------- //
+//Write a JPQL query to list all movie cinemas with higher than a specific date
+    @Query("SELECT mc FROM MovieCinema mc where mc.dateTime = ?1")
+    List<MovieCinema> findAfterSpecificDate(LocalDateTime dateTime);
+
+    // ------------------- Native QUERIES ------------------- //
+//Write a native query to count all movie cinemas by cinema id
+    @Query(value = "SELECT count(*) FROM movie_cinema where cinema_id = ?1",
+            nativeQuery = true)
+    Integer countByMovieCinemaById(Long id);
+//Write a native query that returns all movie cinemas by location name
+    @Query(value = "SELECT * FROM movie_cinema mc join cinema c " +
+            "on mc.id = c.id JOIN location l on c.location_id = l.id " +
+            "where l.name=?1",nativeQuery = true)
+    List<MovieCinema> returnMovieCinemasByLocationName(String locationName);
+
+}
