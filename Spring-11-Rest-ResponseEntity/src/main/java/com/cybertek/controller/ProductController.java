9636eb525e749c0dbba7c9cb970abbad412eab59
GeorgeLondon1 <radac.george@yahoo.com>
2021-01-27 03:27:36 +0200

    Spring REST - Response Entity

--- Spring-11-Rest-ResponseEntity/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-Rest-ResponseEntity/src/main/java/com/cybertek/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.cybertek.controller;
 
 import com.cybertek.entity.Product;
+import com.cybertek.entity.ResponseWrapper;
 import com.cybertek.service.ProductService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -80,8 +81,23 @@ public class ProductController {
         return new ResponseEntity<>(list,map,HttpStatus.OK);
     }
 
+    @GetMapping("/read")
+    public ResponseEntity<ResponseWrapper> readAllProducts(){
+        return ResponseEntity
+                .ok(new ResponseWrapper("products successfully retrieved",
+                        productService.getProducts()));
+    }
 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ResponseWrapper> deleteProduct2(@PathVariable("id") long id){
+        return ResponseEntity.ok(new ResponseWrapper("product successfully deleted"));
+    }
 
+    @DeleteMapping("/delete2/{id}")
+    public ResponseEntity<ResponseWrapper> deleteProduct3(@PathVariable("id") long id){
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .body(new ResponseWrapper("product successfully deleted"));
+    }
 
 
 }
